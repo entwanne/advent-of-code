7a92a3759b37cc110b4c0c41f71f13d8a9855471
Antoine Rozo <antoine.rozo@gmail.com>
2023-12-22 13:17:43 +0100

    D4

---
+++ 2022/d04_p1.py
@@ -0,0 +1,22 @@
+import sys
+
+
+def fullset(a, b):
+    return set(range(a, b+1))
+
+
+def parse(f):
+    for line in f:
+        elfs = line.strip().split(',')
+        yield [
+            fullset(*map(int, elf.split('-')))
+            for elf in elfs
+        ]
+
+
+def overlap(pair):
+    p1, p2 = pair
+    return (p1 & p2) in [p1, p2]
+
+
+print(sum(overlap(pair) for pair in parse(sys.stdin)))
