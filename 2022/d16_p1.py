4e0f6ed18f869a607cda0d66e08c27bb93f0514d
Antoine Rozo <antoine.rozo@gmail.com>
2023-12-23 18:43:43 +0100

    fixup! fixup! D16

--- 2022/d16_p1.py
+++ 2022/d16_p1.py
@@ -16,7 +16,7 @@ def parse(f):
 
 def find_max_path(graph):
     nexts = {0: [('AA', frozenset(), 30, 0, 0)]}
-    seen = set(nexts[0])
+    seen = {('AA', frozenset(), 0, 0)}
 
     while nexts:
         v = max(nexts)
@@ -24,34 +24,102 @@ def find_max_path(graph):
         name, open, n, value, inc = q.pop(0)
         if not q:
             del nexts[v]
+        #print(name, open, n, value, inc)
 
         #print(n)
         if n == 0:
             #print(value)
             #break
+            print(name, open)
             yield value
             continue
 
         node = graph[name]
         children = []
 
-        if name not in open:
-            v = inc + node['rate']
-            new = (name, open | {name}, n-1, value+inc, inc+node['rate'])
-            if new not in seen:
-                nexts.setdefault(v, []).append(new)
-                seen.add(new)
+        if name not in open and node['rate']:
+            key = (name, open | {name}, value+inc, inc+node['rate'])
+            p = (value+inc) * n
+            if key not in seen:
+                nexts.setdefault(p, []).append((name, open | {name}, n-1, value+inc, inc+node['rate']))
+                seen.add(key)
 
         for name_next in node['nexts']:
-            v = inc
-            new = (name_next, open, n-1, value+inc, inc)
-            if new not in seen:
-                nexts.setdefault(v, []).append(new)
-                seen.add(new)
+            key = (name_next, open, value+inc, inc)
+            p = (value+inc) * n
+            if key not in seen:
+                nexts.setdefault(p, []).append((name_next, open, n-1, value+inc, inc))
+                seen.add(key)
 
     #return max(children)
 
 
+def compute_distances(graph, start):
+    distances = {}
+    nexts = [(start, 0)]
+    seen = {start}
+
+    while nexts:
+        name, dist = nexts.pop(0)
+        distances[name] = dist
+
+        node = graph[name]
+        for link in node['nexts']:
+            if link not in seen:
+                nexts.append((link, dist+1))
+                seen.add(link)
+
+    del distances[start]
+    return distances
+
+
+def compute_all_distances(graph):
+    return {
+        name: compute_distances(graph, name)
+        for name in graph
+    }
+
+
+def find_best_path(graph, start='AA'):
+    distances = compute_all_distances(graph)
+    toopen = {name for name, node in graph.items() if node['rate'] > 0}
+    nexts = [('AA', set(), 30, 0, 0)]
+
+    while nexts:
+        name, opened, n, value, inc = nexts.pop(0)
+
+        if n == 0:
+            yield value
+            continue
+
+        #if opened == toopen:
+        #    value += n * inc
+        #    yield value
+        #    continue
+        yield value + n * inc
+
+        if name not in opened and name in toopen:
+            node = graph[name]
+            nexts.append((name, opened | {name}, n-1, value+inc, inc+node['rate']))
+        for link, dist in distances[name].items():
+            if link not in opened and link in toopen and dist <= n:
+                node = graph[link]
+                dist += 1
+                nexts.append((link, opened | {link}, n-dist, value+dist*inc, inc+node['rate']))
+                #nexts.append((link, opened, n-dist, value+dist*inc, inc))
+
+
 graph = {valve['name']: valve for valve in parse(sys.stdin)}
-print(graph)
-print(max(find_max_path(graph)))
+#print(graph)
+#print(max(find_max_path(graph)))
+'''
+n = 0
+for v in find_max_path(graph):
+    print(v)
+    n += 1
+    if n == 20:
+        break
+'''
+#for p in find_best_path(graph):
+#    print(p)
+print(max(find_best_path(graph)))
