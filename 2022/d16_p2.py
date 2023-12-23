1a2dee3adc31cce83efdf1d712365e430289db8a
Antoine Rozo <antoine.rozo@gmail.com>
2023-12-23 20:05:16 +0100

    fixup! D16

--- 2022/d16_p2.py
+++ 2022/d16_p2.py
@@ -43,11 +43,14 @@ def compute_all_distances(graph):
 def find_best_path(graph, start='AA'):
     distances = compute_all_distances(graph)
     toopen = {name for name, node in graph.items() if node['rate'] > 0}
-    nexts = [(start, start, frozenset(), 26, 26, 0, 0, 0, 0)]
-    seen = set(nexts)
+    nexts = {0: [(start, start, frozenset(), 26, 26, 0, 0, 0, 0)]}
+    seen = set(nexts[0])
 
     while nexts:
-        name1, name2, opened, n1, n2, value1, value2, inc1, inc2 = nexts.pop(0)
+        v = max(nexts)
+        name1, name2, opened, n1, n2, value1, value2, inc1, inc2 = nexts[v].pop(0)
+        if not nexts[v]:
+            del nexts[v]
 
         yield value1 + n1 * inc1 + value2 + n2 * inc2
 
@@ -63,7 +66,7 @@ def find_best_path(graph, start='AA'):
                 dist1 += 1
                 new = (link1, name2, opened | {link1}, n1-dist1, n2, value1+dist1*inc1, value2, inc1+node1['rate'], inc2)
                 if new not in seen:
-                    nexts.append(new)
+                    nexts.setdefault(value1+dist1*inc1+(n1-dist1)*(inc1+node1['rate'])+value2+n2*inc2, []).append(new)
                     seen.add(new)
 
         for link2, dist2 in distances[name2].items():
@@ -72,7 +75,7 @@ def find_best_path(graph, start='AA'):
                 dist2 += 1
                 new = (name1, link2, opened | {link2}, n1, n2-dist2, value1, value2+dist2*inc2, inc1, inc2+node2['rate'])
                 if new not in seen:
-                    nexts.append(new)
+                    nexts.setdefault(value1+n1*inc1+value2+dist2*inc2+(n2-dist2)*(inc2+node2['rate']), []).append(new)
                     seen.add(new)
 
 
